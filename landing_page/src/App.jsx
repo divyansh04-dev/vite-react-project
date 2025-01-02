a913ccb1dedeff3eeecbd79161bd2a55abf3623c
Divyansh Jaiswal <work.divyansh7303@gmail.com>
2025-01-02 13:47:04 +0530

    add some pages

--- landing_page/src/App.jsx
+++ landing_page/src/App.jsx
@@ -1,3 +1,8 @@
+import React from "react";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return <></>;
 }
